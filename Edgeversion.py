45223c09640834a3ea7c0921ad12a02017cdd694
Daniel Kristensen <DKristensen88@gmail.com>
2024-09-24 21:36:36 +0200

    iug

--- Edgeversion.py
+++ Edgeversion.py
@@ -43,63 +43,18 @@ hsv_data = []
 
 def get_terrain(tile,tile_x ,tile_y, image_number):
     hsv_tile = cv.cvtColor(tile, cv.COLOR_BGR2HSV)
-    #if x ==0 and y==0:
+
     edge_list = []
     for row_index, row in enumerate(hsv_tile):
-        if 2 < row_index < 97:
+        if 14 < row_index < 85:
             for col_index, hsv in enumerate(row):
-                if col_index<3 or col_index>96:
-                    edge_list.append(hsv.tolist())
+                if col_index<15 or col_index>84:
+                    edge_list.append(hsv.tolist()) #1164 values
         else: 
             for col_index, hsv in enumerate(row):
                 edge_list.append(hsv.tolist())
-        #row4.append(row.tolist())
-    #vertical = hsv_tile[3:-3]
-        #print(vertical[0][:3])
-    #for index, row in enumerate(vertical):
 
-        #row4.append(vertical[index][:3].tolist() + vertical[index][-3:].tolist())
-        
-        #row4.append(vertical[0][:3].tolist())
-        #row4.append(vertical[0][-3:].tolist())
-    #print(f"row4: {len(row4)}")
-    #row4.append(hsv_tile[0].tolist())
-    print(f"row4: {len(edge_list)}")
-    #print(f"row4: {row4[:]}")
-    #print(hsv_tile[0][0])
-    #print(f"hsv: {hsv_tile[0]}")
-    
-        
-        #del row4[3:-3]
-        #row4.append(vertical[0][0:3].tolist())
-        #row4[-1].append(vertical[0][-3:].tolist())
-        #print(vertical[0][0:3])
-        #print(vertical[0][-4:])
-        #print(row4)
-    '''
-        horisontal = []
-        for y, value in enumerate(vertical):
-            #print(y)
-            temp = []
-            
-            for x, hsv in enumerate(value):
-               
-               #print(f"{y}, {x}, {hsv}")
-               if 3 > x:
-                   #print(f"{y}, {x}, {hsv}")
-                   temp.append(hsv.tolist())
-            horisontal.append(temp)
-            print(temp[0])
-        #print(horisontal[0])
-        clean_output = [hsv.tolist() for hsv in horisontal[0][:10]]
-        #print(clean_output)
-        #print(vertical)
-        #print(hsv_tile[-4])
-        #print(f"First three {hsv_tile[0:3]}") #Maybe works
-        #print(f"Last three {hsv_tile[-3:]}") #Maybe works
-        #print(f"test {hsv_tile[3:-4]}") #Not working
-        #print(f"test {hsv_tile[0][-3:]}") #Not working
-    '''
+    print(f"edge_list: {len(edge_list)}")
 
     hue, saturation, value = np.median(edge_list, axis=(0)) # Consider using median instead of mean
     hue = round(hue, 5)
