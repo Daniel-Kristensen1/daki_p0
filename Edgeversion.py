7be78bc0b217ec4dd9ce7667ca9a87c652acb222
Daniel Kristensen <DKristensen88@gmail.com>
2024-10-07 20:46:54 +0200

    test commit

--- Edgeversion.py
+++ Edgeversion.py
@@ -63,25 +63,27 @@ def get_terrain(tile,tile_x ,tile_y, image_number):
     #hue, saturation, value = np.mean(hsv_tile, axis=(0,1))
     print(f"H: {hue}, S: {saturation}, V: {value}")
     hsv_data.append({"Image number": f"{image_number-1}","Coordinate_x": f"{tile_x}","Coordinate_y": f"{tile_y}", "Hue": hue, "Saturation": saturation, "Value": value}) #Adds data to the dataset
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 23 <= hue <= 26 and 224 <= saturation <= 254 and 153 <= value <= 205:
         return "Field"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
-        return "Forest"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
-        return "Lake"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 36 <= hue <= 49 and 188 <= saturation <= 250 and 74 <= value <= 162:
         return "Grassland"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 32 <= hue <= 45 and 92 <= saturation <= 237 and 38 <= value <= 75:
+        return "Forest"
+    if 105 <= hue <= 109 and 219 <= saturation <= 255 and 107 <= value <= 199:
+        return "Ocean"
+    if 18 <= hue <= 26 and 66 <= saturation <= 182 and 80 <= value <= 147:
         return "Swamp"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 20 <= hue <= 30 and 31 <= saturation <= 89 and 17 <= value <= 54:
         return "Mine"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
-        return "Home"
+    if 17 <= hue <= 40 and 41 <= saturation <= 187 and 53 <= value <= 133:
+        return "Home" #home
     return "Unknown"
 
 if __name__ == "__main__":
     pic= [1, 2, 3, 12, 13, 14, 23, 24, 25, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 55, 56, 57, 65, 66, 67]
-    for i in pic:
+    pic1 = [1]
+    pic2 = [i+1 for i in range(74)]
+    for i in pic2:
         main(i)
 
 
